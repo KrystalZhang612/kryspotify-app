c238dc5ff055cb882535b68ce66c4c80d247d559
Krystal Zhang <72481348+KrystalZhang612@users.noreply.github.com>
2022-12-08 20:11:09 -0800

    last commit

--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -5,10 +5,8 @@ import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 const TopCharts = () => {
     const { activeSong, isPlaying } = useSelector((state) => state.player);
     const { data, isFetching, error } = useGetTopChartsQuery();
-
     if (isFetching) return <Loader title="Loading top charts..." />;
     if (error) return <Error />;
-
     return (
         <div className="flex flex-col">
             <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
