ed70752e79dc2be91382559b685f4d19317893de
Krystal Zhang <72481348+KrystalZhang612@users.noreply.github.com>
2022-12-08 20:04:27 -0800

    last commit

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -26,7 +26,6 @@ const DetailsHeader = ({ artistId, artistData, songData }) => (
             <p className="text-base text-gray-400 mt-2">{songData?.subtitle}</p>
           </Link>
         )}
-
         <p className="text-base text-gray-400 mt-2">
           {artistId
             ? artistData?.artists[artistId].attributes?.genreNames[0]
